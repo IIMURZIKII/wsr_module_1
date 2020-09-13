6b560f7c0ebf90ed45e0a839745c7b42652d9f33
IIMURZIKII <skyrim-love127@yandex.ru>
2020-09-13 12:41:37 +0500

    Save

--- index.php
+++ index.php
@@ -1,8 +1,34 @@
 <?php
 
-// Заголовки на ответ
+// Заголовки на ответ.
 header("Content-Type: application/json; charset=utf-8");
 
-header('HTTP/1.0 201 created');
+if (isset($_GET['api'])) {
 
-echo 1;
+    if (isset($_GET['signup'])) {
+
+        $array = array('id' => 1);
+        header('HTTP/1.0 201 Created');
+        api_response($array);
+    }
+
+    else {
+
+        header('HTTP/1.0 404 Not Found');
+        exit;
+    }
+}
+
+else {
+
+    header('HTTP/1.0 404 Not Found');
+    exit;
+}
+
+exit;
+
+function api_response($array) {
+    header("Content-Type: application/json; charset=utf-8");
+    echo json_encode($array);
+    exit;
+}
